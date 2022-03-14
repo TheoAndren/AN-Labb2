c82c3d1c4f308324ab60b8d7212d1b341c8bb1c8
TheoAndren <theo.andren@gmail.com>
2022-03-14 13:07:38 +0100

    updated queries

--- AN Labb2/Program.cs
+++ AN Labb2/Program.cs
@@ -1,6 +1,7 @@
 ﻿using AN_Labb2.Models;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace AN_Labb2
 {
@@ -8,14 +9,45 @@ namespace AN_Labb2
     {
         static void Main(string[] args)
         {
-            var context = new SchoolDbContext();
+            using SchoolDbContext context = new SchoolDbContext();
 
 
+            // Ta fram lärare som undervisar matte
+            var c = from k in context.Teachers where k.SubjectId == 6 select k;
 
+            foreach (var item in c)
+            {
+                Console.WriteLine("Lärare som undervisar kursen Math 1");
+                Console.WriteLine("");
+                Console.WriteLine("{0}", item.TeacherName);
+                Console.WriteLine("------------------------------------------");
+            }
 
+            //Kolla om tabellen subjects innehåller Programmering 1
+            var query =
+               from x in context.Subjects
+               where x.SubjectName.Contains("Programming 1")
+               orderby x.SubjectName
+               select x;
+           
+            foreach (var course in query)
+            {
+                Console.WriteLine("");
+                Console.WriteLine(" The Course Programming 1 does exist");
+                Console.WriteLine("------------------------------------------");
+            }
 
+            // Hämta elever med alla sina lärare
+            var courses =
+                from v in context.Courses
+                join a in context.Teachers on v.CourseId equals a.CourseId
+                join b in context.Students on v.CourseId equals b.CourseId
+                select new { StudentName = b.StudentName, TeacherName = a.TeacherName};
 
-
+            foreach (var item in courses)
+            {
+                Console.WriteLine("Student: " + item.StudentName + " " + "Teacher: " + item.TeacherName);
+            }
 
 
 
@@ -32,46 +64,46 @@ namespace AN_Labb2
             //context.SaveChanges();
 
 
-            //            var students = new List<Student>{
-            //    new Student { StudentName = "Kalle Persson" , CourseId = 1},
-            //    new Student { StudentName = "Carl Gustaffsson" , CourseId = 2},
-            //    new Student { StudentName = "Anja Persson" , CourseId = 3},
-            //    new Student { StudentName = "Sven Karlsson" , CourseId = 4},
-            //    new Student { StudentName = "Olle Klementin" , CourseId = 4},
-            //    new Student { StudentName = "Oscar Oscarsson" , CourseId = 1 }
-            //}; context.AddRange(students);
-            //            context.SaveChanges();
+               //            var students = new List<Student>{
+               //    new Student { StudentName = "Kalle Persson" , CourseId = 1},
+               //    new Student { StudentName = "Carl Gustaffsson" , CourseId = 2},
+               //    new Student { StudentName = "Anja Persson" , CourseId = 3},
+               //    new Student { StudentName = "Sven Karlsson" , CourseId = 4},
+               //    new Student { StudentName = "Olle Klementin" , CourseId = 4},
+               //    new Student { StudentName = "Oscar Oscarsson" , CourseId = 1 }
+               //}; context.AddRange(students);
+               //            context.SaveChanges();
 
 
 
 
-            //var teachers = new List<Teacher>{
-            //    new Teacher { TeacherName = "Karl Harryson" , CourseId = 1, SubjectId = 1},
-            //    new Teacher { TeacherName = "Tobias Qlok" , CourseId = 1, SubjectId = 2},
-            //    new Teacher { TeacherName = "Anas Qlok" , CourseId = 1, SubjectId = 4},
-            //    new Teacher { TeacherName = "Reidar Qlok" , CourseId = 1, SubjectId = 5},
-            //    new Teacher { TeacherName = "Billy Sneider" , CourseId = 2, SubjectId = 7},
-            //    new Teacher { TeacherName = "Magdalena Svensson" , CourseId = 3, SubjectId = 3},
-            //    new Teacher { TeacherName = "Birgitta Karlsson" , CourseId = 3, SubjectId = 8},
-            //    new Teacher { TeacherName = "Ola Konradsson" , CourseId = 4, SubjectId = 6}
-            //}; context.AddRange(teachers);
-            //context.SaveChanges();
+               //var teachers = new List<Teacher>{
+               //    new Teacher { TeacherName = "Karl Harryson" , CourseId = 1, SubjectId = 1},
+               //    new Teacher { TeacherName = "Tobias Qlok" , CourseId = 1, SubjectId = 2},
+               //    new Teacher { TeacherName = "Anas Qlok" , CourseId = 1, SubjectId = 4},
+               //    new Teacher { TeacherName = "Reidar Qlok" , CourseId = 1, SubjectId = 5},
+               //    new Teacher { TeacherName = "Billy Sneider" , CourseId = 2, SubjectId = 7},
+               //    new Teacher { TeacherName = "Magdalena Svensson" , CourseId = 3, SubjectId = 3},
+               //    new Teacher { TeacherName = "Birgitta Karlsson" , CourseId = 3, SubjectId = 8},
+               //    new Teacher { TeacherName = "Ola Konradsson" , CourseId = 4, SubjectId = 6}
+               //}; context.AddRange(teachers);
+               //context.SaveChanges();
 
 
 
 
 
-            //            var subjects = new List<Subject>{
-            //    new Subject { SubjectName = "OOP" , CourseId = 1},
-            //    new Subject { SubjectName = "Programming with C#" , CourseId = 1},
-            //    new Subject { SubjectName = "HRL" , CourseId = 3},
-            //    new Subject { SubjectName = "Programming 1" , CourseId = 1},
-            //    new Subject { SubjectName = "Porgramming 2" , CourseId = 1},
-            //    new Subject { SubjectName = "Nature Science" , CourseId = 4},
-            //    new Subject { SubjectName = "Software Development 1" , CourseId = 2},
-            //    new Subject { SubjectName = "CPR" , CourseId = 3}
-            //}; context.AddRange(subjects);
-            //            context.SaveChanges();
+               //            var subjects = new List<Subject>{
+               //    new Subject { SubjectName = "OOP" , CourseId = 1},
+               //    new Subject { SubjectName = "Programming with C#" , CourseId = 1},
+               //    new Subject { SubjectName = "HRL" , CourseId = 3},
+               //    new Subject { SubjectName = "Programming 1" , CourseId = 1},
+               //    new Subject { SubjectName = "Porgramming 2" , CourseId = 1},
+               //    new Subject { SubjectName = "Math 1" , CourseId = 4},
+               //    new Subject { SubjectName = "Software Development 1" , CourseId = 2},
+               //    new Subject { SubjectName = "CPR" , CourseId = 3}
+               //}; context.AddRange(subjects);
+               //            context.SaveChanges();
 
         }
     }
